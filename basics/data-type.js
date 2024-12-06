d275cb3ccf27c446863a0f43eb3e7efe4f175220
Riddhi-Gohel <121100616+Riddhi-Gohel@users.noreply.github.com>
2024-12-06 07:27:58 +0000

    data-type

---
+++ basics/data-type.js
@@ -0,0 +1,23 @@
+/*
+String
+Number
+Bigint
+Boolean
+Undefined
+Null
+Symbol
+Object
+
+*/
+
+let name = "Riddhi"
+console.log(typeof "Riddhi")
+
+let accnum = 1542369854
+console.log(typeof accnum);
+
+let accname
+console.log(typeof accname);
+
+console.log(typeof "riddhi")
+
