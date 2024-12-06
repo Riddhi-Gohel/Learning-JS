36db1e845b5f391800fbb1dfd3fc18edfbcf359d
Riddhi-Gohel <121100616+Riddhi-Gohel@users.noreply.github.com>
2024-12-06 06:37:38 +0000

    variables

---
+++ test/variables.js
@@ -0,0 +1,12 @@
+const accountid = 123456
+let gmail = "gohelr@gmail.com"
+var password = 1704
+username = ""
+//companyname;// use var or let keyword otherwise show error companyname is not defined
+let companyname;
+
+
+//accountid = 1704// const are not changed
+
+console.log(accountid)
+console.table([accountid, gmail, password, username, companyname])
